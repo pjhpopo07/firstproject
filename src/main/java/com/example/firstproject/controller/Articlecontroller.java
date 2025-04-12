0b42fdbdf165d1b18fc11c0b2507f88286c3d4dd
pjhpopo07 <pjhpopo07@gmail.com>
2025-04-12 17:10:10 +0900

    feat: first commit

--- src/main/java/com/example/firstproject/controller/Articlecontroller.java
+++ src/main/java/com/example/firstproject/controller/Articlecontroller.java
@@ -1,12 +1,19 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.firstproject.dto.ArticleForm;
 
 @Controller
+@Slf4j // 로깅을 위한 골뱅이(어노테이션)
 public class Articlecontroller {
+    @Autowired //스프링 부트가 미리 생성해놓은 객체를 가져다가 자동 연결해준다.
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm(){
@@ -15,7 +22,19 @@ public class Articlecontroller {
     }
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
-        return "";
+        log.info(form.toString());
+     /*System.out.println(form.toString());
+     --> 실제 서버에서는 쓰지 않는다 악영향을 주기 때문에 --> 로깅으로 대체*/
+        //1.DTO를 변환 -> Entity
+        Article article = form.toEntity();
+        System.out.println(article);
+
+        //2.Repository에게 Entity를 DB안에 저장하게 한다.
+        Article saved=articleRepository.save(article);
+        log.info(saved.toString());
+        //System.out.println(saved.toString()); --> 이것도 로깅으로 대체
+        //articleRepository 인식할 수 있어 기본적으로 제공하는 save기능 사용가능
+        //articleRepository 객체를 선언을 안해도 spring 자체에서 해준다.
+        return"";
     }
 }
--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,20 +1,21 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+
+@AllArgsConstructor
+//생성자를 적지 않고 똑같은 효과를 내준다.
+@ToString
+//To.String 적지 않고도 똑같은 효과를 내준다.
 public class ArticleForm {
 
     private String title;
     private String content;
 
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
+    public Article toEntity() {
+     return new Article(null, title, content);
     }
 
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
